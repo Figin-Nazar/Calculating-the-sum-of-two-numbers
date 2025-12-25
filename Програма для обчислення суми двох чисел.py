e45d5b356f1d361bae118f3f5c513dbf08a299d8
Figin Nazar <gesahtgg@gmail.com>
2025-12-25 18:14:10 +0200

    Add files via upload

---
+++ Програма для обчислення суми двох чисел.py
@@ -0,0 +1,9 @@
+def sum_numbers(a, b):
+    return a + b 
+print("Програма для обчислення суми двох чисел")
+
+a = float(input("Введіть перше число: "))
+b = float(input("Введіть друге число: "))
+
+result = sum_numbers(a, b)
+print("Сума чисел: =", result)
